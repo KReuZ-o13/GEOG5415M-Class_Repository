dc38e6114511ec00d816fc91683aae0f0e7b8ea3
kathy117 <bwzt0949@leeds.ac.uk>
2025-12-02 15:33:18 +0000

    Update points.py

--- points.py
+++ points.py
@@ -23,8 +23,7 @@ points_list = [
     [  53.800622, -1.5483202, "The Leeds Art Gallery!" ],
     [  53.798874, -1.5408800, "Rayan's Favourite Japanese Restaurant" ],
     [  34.58223767715908, 135.79883198965894, "Kuni's favourite ramen restaurant in Japan"]
-
-]
+    [  53.79755036197394, -1.5433104537008813,"Kathy's favourite restaurant"]
 
 def get_points():
     """Return the list of points"""
